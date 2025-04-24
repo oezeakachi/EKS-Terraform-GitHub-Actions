bc06491128e53bd2849a1220d2ce2b41803f5b3e
oezeakachi <oezeakac@redhat.com>
2025-04-24 17:56:40 +0100

    missed out config

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-obi-tf-bucket"
     region         = "eu-west-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
